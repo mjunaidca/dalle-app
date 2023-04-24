cc839caa6497ead495348595c9fb7b4dc010e0d2
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-04-24 20:54:01 +0500

    Added Download option in share age button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React from "react";
+import FileSaver from "file-saver";
 
 const Button = ({ imageUrl }: { imageUrl: string }) => {
-  return <div className="btn mt-2">Button</div>;
+  return <div className="btn mt-2" onClick={() => FileSaver.saveAs(imageUrl, imageUrl)}>Download Now</div>;
 };
 
 export default Button;
